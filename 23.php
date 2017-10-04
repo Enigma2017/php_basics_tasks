b1859ca69db1f435216d77b76ab292d730ed7252
Enigma2017 <32341036+Enigma2017@users.noreply.github.com>
2017-10-04 14:25:52 +0200

    Add files via upload
    full home task

---
+++ 23.php
@@ -0,0 +1,8 @@
+<?php
+#Напишите 3 строки, используя три разные функции для вывода текста
+$a = "first line";
+print_r ($a);
+echo "second line";
+print "third line"; 
+?>
+ 
---
+++ 24.php
@@ -0,0 +1,9 @@
+<?php
+#Создайте в файле 3 комментария тремя разными типами
+echo "comments"
+// first comment
+# second comment
+/*third
+long
+comment*/
+?>
